b6fcb4c815203a212295231c1dfcfe0fc97697e5
DeviTrianiOctavia08 <devitriani1915@gmail.com>
2023-08-25 20:21:00 +0700

    Tugas-4

--- 4_Branching-Looping-Function/praktikum/Soal-prioritas-2-bagian-2-Looping.dart
+++ 4_Branching-Looping-Function/praktikum/Soal-prioritas-2-bagian-2-Looping.dart
@@ -1,4 +1,32 @@
-//SOAL PRIORITAS 1
+//SOAL PRIORITAS 2
 //Tugas Perulangan (Looping)
 
 //Buatlah sebuah jam pasir 
+
+import 'dart:io';
+
+void main() {
+  int tinggi = 5; // Jumlah baris
+  
+  // Bagian atas jam pasir
+  for (int i = tinggi -1; i >= 1; i--) {
+    for (int j = 1; j <= tinggi - i; j++) {
+      stdout.write(" ");
+    } 
+    for (int k = 1; k <= 2 * i - 1; k++) {
+      stdout.write("*");
+    }
+    print('');
+  }
+ 
+  // Bagian bawah jam pasir
+  for (var i = 1; i <= tinggi; i++) {
+    for (var j = 1; j <= tinggi - i; j++) {
+      stdout.write(" ");
+    }
+    for (var k = 1; k <= 2 * i - 1; k++) {
+      stdout.write("*");
+    }
+    print('');
+  }
+}
