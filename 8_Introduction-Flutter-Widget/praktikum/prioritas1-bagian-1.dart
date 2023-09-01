cb9f692dea8255837c543cd20da0af2b1099bcfe
DeviTrianiOctavia08 <devitriani1915@gmail.com>
2023-09-01 23:22:32 +0700

    Tugas-8

---
+++ 8_Introduction-Flutter-Widget/praktikum/prioritas1-bagian-1.dart
@@ -0,0 +1,8 @@
+// Soal Prioritas 1 bagian 1
+
+// Sebutkan 3 aplikasi yang dibangun menggunakan teknologi Flutter! 
+// 1. Flutter Gallery: Flutter Gallery adalah aplikasi demonstrasi resmi dari Flutter yang dirancang untuk menunjukkan berbagai widget dan fitur yang dapat digunakan dalam pengembangan aplikasi Flutter. Ini merupakan referensi yang berguna bagi pengembang Flutter untuk memahami berbagai konsep dan kemampuan Flutter.
+
+// 2. Alibaba Group's Xianyu: Xianyu adalah aplikasi e-commerce populer di Tiongkok yang dimiliki oleh Alibaba Group. Mereka menggunakan Flutter dalam pengembangan aplikasi seluler mereka. Pengguna dapat membeli dan menjual barang-barang bekas melalui aplikasi ini.
+
+// 3. Google Ads: Google Ads, yang dulu dikenal sebagai AdWords, adalah platform periklanan online dari Google. Mereka telah menggunakan Flutter dalam pengembangan aplikasi seluler Google Ads, yang memungkinkan pengiklan untuk mengelola kampanye iklan mereka secara efisien.
---
+++ 8_Introduction-Flutter-Widget/praktikum/prioritas1-bagian-2.dart
@@ -0,0 +1,10 @@
+// Soal Prioritas 1 bagian 2
+
+// Tampilkan informasi software yang dibutuhkan oleh Flutter dengan menggunakan perintah Flutter doctor!
+
+// Status Flutter SDK 
+// Status Android toolchain 
+// Status Xcode 
+// Status Chrome 
+// Status emulator 
+// Status IDE 
